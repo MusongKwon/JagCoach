5134b131971ebe083b9c4bc47b43bfeeb5051b1d
MusongKwon <156956738+MusongKwon@users.noreply.github.com>
2025-03-05 14:52:15 -0600

    Update JagCoachFileAnalysis.py

--- JagCoachFileAnalysis.py
+++ JagCoachFileAnalysis.py
@@ -2,9 +2,7 @@ import os
 from pydub import AudioSegment
 from audio_extract import extract_audio
 from config import config  # 🔹 Import centralized config settings
-
 mysp = __import__("my-voice-analysis")
-
 
 def process_video(file_path):
     if not os.path.exists(file_path):
@@ -25,27 +23,62 @@ def process_video(file_path):
         sound = sound.set_frame_rate(config.SAMPLE_RATE).set_channels(1)
 
         # Export the clean WAV file
-        sound.export(output_path, format="wav")
+        sound.export(output_path, format="wav", codec="pcm_s16le")
         print(f"Cleaned audio file saved: {output_path}")
 
         return output_path
     except Exception as e:
         raise RuntimeError(f"Error processing audio file '{file_path}': {e}")
 
+# Get gender of presenter
+def get_gender_mood(file_path):
+    p = os.path.splitext(os.path.basename(file_path))[0]  # Extract filename
+    c = os.getcwd()+os.path.join(config.UPLOAD_FOLDER, "\\uploads\\processed_audio\\")  # Use config path
+    return mysp.myspgend(p, c)
+
+# Get pronunciation score
+def get_pronunciation_score(file_path):
+    p = os.path.splitext(os.path.basename(file_path))[0]  # Extract filename
+    c = os.getcwd()+os.path.join(config.UPLOAD_FOLDER, "\\uploads\\processed_audio\\")  # Use config path
+    return mysp.mysppron(p, c)
+
+# Get speech rate
+def get_speech_rate(file_path):
+    p = os.path.splitext(os.path.basename(file_path))[0]  # Extract filename
+    c = os.getcwd()+os.path.join(config.UPLOAD_FOLDER, "\\uploads\\processed_audio\\")  # Use config path
+    return mysp.myspsr(p, c)
+
+# Get articulation rate
+def get_articulation_rate(file_path):
+    p = os.path.splitext(os.path.basename(file_path))[0]  # Extract filename
+    c = os.getcwd()+os.path.join(config.UPLOAD_FOLDER, "\\uploads\\processed_audio\\")  # Use config path
+    return mysp.myspatc(p, c)
+
+# Get ratio of speaking time excl. fillers/pauses and total speaking time
+def get_speaking_time_ratio(file_path):
+    p = os.path.splitext(os.path.basename(file_path))[0]  # Extract filename
+    c = os.getcwd()+os.path.join(config.UPLOAD_FOLDER, "\\uploads\\processed_audio\\")  # Use config path
+    return mysp.myspbala(p, c)
 
 # Get pauses in the speech
 def get_pauses(file_path):
     p = os.path.splitext(os.path.basename(file_path))[0]  # Extract filename
-    c = os.path.join(config.UPLOAD_FOLDER, "cleaned_audio") + "/"  # Use config path
+    c = os.getcwd()+os.path.join(config.UPLOAD_FOLDER, "\\uploads\\processed_audio\\")  # Use config path
     return mysp.mysppaus(p, c)
 
+
 if __name__ == "__main__":
-    test_file = os.path.join(config.UPLOAD_FOLDER, "CybSecCapstone.mp4")
+    test_file = os.path.join(config.UPLOAD_FOLDER, "vid1.mp4")
 
     try:
         wav_file = process_video(test_file)
         print(f"Audio extracted successfully: {wav_file}")
-        print(get_pauses(wav_file))
+        get_pauses(wav_file)
+        get_gender_mood(wav_file)
+        get_pronunciation_score(wav_file)
+        get_speech_rate(wav_file)
+        get_articulation_rate(wav_file)
+        get_speaking_time_ratio(wav_file)
 
     except Exception as e:
         print(f"Error: {e}")
