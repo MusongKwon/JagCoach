75a31ec699b2d9e5d924c8a2d349759403d5f382
Caleb Holt <143446391+CalebH187@users.noreply.github.com>
2025-03-05 14:26:03 -0600

    Parses all files in directory mp4 -> wav then, wav -> transcribe finally, transcribe -> .txt file

--- JagCoachUI/GUI4JagCoach.py
+++ JagCoachUI/GUI4JagCoach.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
 import os
-from JagCoachUI.MP4toWAV import convert_mp4_to_wav
+from JagCoachUI.MP4toWAV import convert_all_mp4_to_wav
 import sys
+
 sys.stdout.flush()
 
-
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+# Ensure the upload folder exists
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 # Route for the main page
 @app.route("/", methods=["GET", "POST"])
@@ -14,20 +19,26 @@ def index():
         # Retrieve the file from the form
         file = request.files.get("video_file")
         if file:
-            file = request.files.get("video_file")
-            file_path = os.path.join("uploads", file.filename)
+            file_path = os.path.join(UPLOAD_FOLDER, file.filename)
             file.save(file_path)
-
-            wav_path = os.path.splitext(file_path)[0] + ".wav"
 
             print(f"File received: {file_path}", flush=True)  # Debugging print statement
             print("Calling convert_mp4_to_wav", flush=True)  # Debugging print statement
 
+        processed_files = []
+        for filename in os.listdir(UPLOAD_FOLDER):
+            if filename.endswith(".mp4"):
+                mp4_path = os.path.join(UPLOAD_FOLDER, filename)
+                wav_path = os.path.splitext(UPLOAD_FOLDER)[0] + ".wav"
+
+                print(f"Processing: {mp4_path}", flush=True)
+
             try:
-                convert_mp4_to_wav(file_path, wav_path)
-                print(f"Conversion done, file saved at: {wav_path}")  # Debugging print
+                convert_all_mp4_to_wav(mp4_path, wav_path)
+                processed_files.append(wav_path)
+                print(f"Conversion done, {wav_path}", flush=True)  # Debugging print
             except Exception as e:
-                print(f"Error during conversion: {e}", flush=True)
+                print(f"Error during conversion {mp4_path}: {e}", flush=True)
 
             return render_template("index.html", message=f"File '{file.filename}' uploaded successfully!", file_path=file_path)
 
