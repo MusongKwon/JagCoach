4d99ba00353797d1ffc7c821e864d237aa60572a
Jesus Lopez <jesusmax313@gmail.com>
2025-03-07 15:33:56 -0600

    LLM Integrated with File Analysis

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "supersecretkey"
-    UPLOAD_FOLDER = "uploads"
+    UPLOAD_FOLDER = "JagCoach-IncludingAudioStats/uploads"
     MAX_CONTENT_LENGTH = 50 * 1024 * 1024  # 50MB file upload limit
     SAMPLE_RATE = 44000 # added sample rate. Originally from file_analysis.py
 
