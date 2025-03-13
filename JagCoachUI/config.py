5ea0bde053e30257fb4265cb7131f4b9d41414b2
MusongKwon <156956738+MusongKwon@users.noreply.github.com>
2025-03-13 10:59:06 -0500

    Update config.py

--- JagCoachUI/config.py
+++ JagCoachUI/config.py
@@ -3,7 +3,7 @@ import os
 class config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "supersecretkey"
     UPLOAD_FOLDER = "uploads"
-    MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 50MB file upload limit
+    MAX_CONTENT_LENGTH = 1000 * 1024 * 1024  # 50MB file upload limit
     SAMPLE_RATE = 44000 # added sample rate. Originally from file_analysis.py
 
 
