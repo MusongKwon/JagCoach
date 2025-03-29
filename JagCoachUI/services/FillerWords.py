cc8751286d075745a3771f9152acdcfb2d6b0f7f
MusongKwon <156956738+MusongKwon@users.noreply.github.com>
2025-03-29 13:07:50 -0500

    Update FillerWords.py

--- JagCoachUI/services/FillerWords.py
+++ JagCoachUI/services/FillerWords.py
@@ -1,12 +1,12 @@
 import os
 from JagCoachUI.config import config
 
-def get_filler_word_ratio(transcript_path):
+def get_filler_word_ratio(transcript_text):
     dictionary_path = os.path.join(os.getcwd(), config.UPLOAD_FOLDER, "dictionary")
     filler_dictionary_path = os.path.join(dictionary_path, "filler_words.txt")
 
     dictionary = load_dictionary(filler_dictionary_path)
-    stats = check_words_in_file(transcript_path, dictionary)
+    stats = check_words_in_file(transcript_text, dictionary)
 
     processed_audio_path = os.path.join(os.getcwd(), config.UPLOAD_FOLDER, "processed_audio")
     filler_ratio_path = os.path.join(processed_audio_path, "filler_word_ratio.txt")
@@ -27,19 +27,14 @@ def load_dictionary(dictionary_path):
         return set(word.strip().lower() for word in file)
 
 
-def check_words_in_file(file_path, dictionary):
-    """Check each word in a file against the dictionary."""
-    total_word_count = 0
+def check_words_in_file(transcription_text, dictionary):
+    words = transcription_text.strip().split()
+    total_word_count = len(words)
     filler_word_count = 0
-
-    with open(file_path, 'r', encoding='utf-8') as file:
-        for line in file:
-            words = line.strip().split()
-            total_word_count += len(words)
-            for word in words:
-                cleaned_word = word.lower().strip('.,!?()[]{}":;')  # Remove punctuation
-                if cleaned_word in dictionary:
-                    filler_word_count += 1
+    for word in words:
+        cleaned_word = word.lower().strip('.,!?()[]{}":;')
+        if cleaned_word in dictionary:
+            filler_word_count += 1
 
     # Avoid division by zero if file is empty
     if total_word_count == 0:
