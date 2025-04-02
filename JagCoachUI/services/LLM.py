5eeebd84405995bd8d11f962d468505a1e5c73d8
MusongKwon <156956738+MusongKwon@users.noreply.github.com>
2025-04-02 14:15:12 -0500

    Update LLM.py

--- JagCoachUI/services/LLM.py
+++ JagCoachUI/services/LLM.py
@@ -1,34 +1,26 @@
-from JagCoachUI.services.JagCoachFileAnalysis import get_elements_dictionary
-from JagCoachUI.services.FaceAnalysis import process_video
 from ollama import chat
 from ollama import ChatResponse
 
-def evaluate_speech(mp4_file, interactive_mode=False):
-    emotion_ratio,  eye_contact_ratio = process_video(mp4_file)
-    student_results = get_elements_dictionary(emotion_ratio, eye_contact_ratio)
-    print(student_results)
+def evaluate_speech(student_results, interactive_mode=False):
+    
     final_grade = f"You scored a {student_results['final_grade']} out of a 100!\n"
 
     if student_results['mood'] is None:
         student_results_str = f"""{{
-            pronunciation score: {student_results['pronunciation_score']} out of 20,
-            speech rate score: {student_results['speech_rate']} out of 9,
-            articulation score: {student_results['articulation_rate']} out of 14,
-            pauses score: {student_results['speaking_ratio']} out of 14,
-            filler word score: {student_results['filler_word_ratio']} out of 20,
-            facial expression score: {student_results['emotion_ratio']} out of 6,
-            eye contact score: {student_results['eye_contact_ratio']} out of 17
+            pronunciation score: {student_results['pronunciation_score']} out of 10,
+            speech rate score: {student_results['speech_rate']} out of 10,
+            filler word score: {student_results['filler_word_ratio']} out of 10,
+            facial expression score: {student_results['emotion_ratio']} out of 10,
+            eye contact score: {student_results['eye_contact_ratio']} out of 10
         }}"""
     else:
         student_results_str = f"""{{
             delivery score: {student_results['mood']} out of 10,
-            pronunciation score: {student_results['pronunciation_score']} out of 18,
-            speech rate score: {student_results['speech_rate']} out of 8,
-            articulation score: {student_results['articulation_rate']} out of 13,
-            pauses score: {student_results['speaking_ratio']} out of 13,
-            filler word score: {student_results['filler_word_ratio']} out of 18,
-            facial expression score: {student_results['emotion_ratio']} out of 5,
-            eye contact score: {student_results['eye_contact_ratio']} out of 15
+            pronunciation score: {student_results['pronunciation_score']} out of 10,
+            speech rate score: {student_results['speech_rate']} out of 10,
+            filler word score: {student_results['filler_word_ratio']} out of 10,
+            facial expression score: {student_results['emotion_ratio']} out of 10,
+            eye contact score: {student_results['eye_contact_ratio']} out of 10
         }}"""
 
     # Initialize conversation history
