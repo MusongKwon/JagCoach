0bcc7774315eb4b92c6437cab22dfc71547dbb05
MusongKwon <156956738+MusongKwon@users.noreply.github.com>
2025-04-02 15:41:53 -0500

    Update FillerWords.py

--- JagCoachUI/services/FillerWords.py
+++ JagCoachUI/services/FillerWords.py
@@ -1,22 +1,8 @@
-import os
-from JagCoachUI.config import config
-
 def get_filler_word_ratio(transcript_text):
-    dictionary_path = os.path.join(os.getcwd(), config.UPLOAD_FOLDER, "dictionary")
-    filler_dictionary_path = os.path.join(dictionary_path, "filler_words.txt")
-
-    dictionary = load_dictionary(filler_dictionary_path)
-    stats = check_words_in_file(transcript_text, dictionary)
-
-    return stats
-
-def load_dictionary(dictionary_path):
-    """Load words from a dictionary file into a set."""
-    with open(dictionary_path, 'r', encoding='utf-8') as file:
-        return set(word.strip().lower() for word in file)
-
-def check_words_in_file(transcription_text, dictionary):
-    words = transcription_text.strip().split()
+    dictionary = {'just', 'accordingly', 'so', 'um', 'er', 'very', 'right', 'hmm', 'about', 'okay', 
+                  'literally', 'ah', 'actually', 'well', 'yeah', 'like', 'absolutely', 'umm', 'basically', 'totally'}
+    
+    words = transcript_text.strip().split()
     total_word_count = len(words)
     filler_word_count = 0
     for word in words:
