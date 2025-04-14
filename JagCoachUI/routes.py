b0c35a62742cdaaa2942fb6203d68fa78a3b598f
MusongKwon <156956738+MusongKwon@users.noreply.github.com>
2025-04-14 13:58:10 -0500

    Update routes.py

--- JagCoachUI/routes.py
+++ JagCoachUI/routes.py
@@ -6,11 +6,12 @@ from JagCoachUI.services.LLM import evaluate_speech
 from JagCoachUI.services.WhisperCall import get_transcript
 from JagCoachUI.services.FillerWords import get_filler_word_ratio
 from JagCoachUI.config import config
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 processed_audio_path = ""
 filler_ratio = 0.0
 executor = ThreadPoolExecutor(max_workers=2)
+process_executor = ProcessPoolExecutor(max_workers=1)
 processing_results = {}
 
 main_bp = Blueprint("main", __name__)
@@ -19,7 +20,6 @@ main_bp = Blueprint("main", __name__)
 @main_bp.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # Get the video file
         file = request.files.get("video_file")
         if file:
             upload_folder = current_app.config["UPLOAD_FOLDER"]
@@ -29,16 +29,14 @@ def index():
 
             if os.path.exists(file_path):
                 os.remove(file_path)
-                print(f"Existing file '{file_path}' deleted.")
+                #print(f"Existing file '{file_path}' deleted.")
 
             try:
                 file.save(file_path)
-                print(f"Video uploaded successfully: {file_path}")
+                #print(f"Video uploaded successfully: {file_path}")
             except Exception as e:
                 print(f"Error saving file: {e}")
-            print(f"Video uploaded successfully: {file_path}")
 
-            #  Process the video file for auditory and facial analysis
             global processed_audio_path
             processed_audio_path = process_video(file_path)
 
@@ -55,10 +53,10 @@ def transcribe():
     try:
         global processed_audio_path
 
-        # Transcribe the audio to calculate filler ratio
         transcription_text = get_transcript(processed_audio_path)
         global filler_ratio
         filler_ratio = get_filler_word_ratio(transcription_text)
+        print("filler ratio: ", filler_ratio)
 
         print("Transcription complete bossman")
         return jsonify({"transcription": transcription_text})
@@ -70,18 +68,19 @@ def transcribe():
 def evaluate():
     print(f"Evaluate has been summoned")
     try:
-        # Calculate the auditory analysis metrics
         global processed_audio_path
         global filler_ratio
-        elements = get_elements(processed_audio_path)
+        future_elements = process_executor.submit(get_elements, processed_audio_path)
         
         future = processing_results.get("face_analysis")
         if future:
             emotion_ratio, eye_contact_ratio = future.result()
-        else:
-            emotion_ratio, eye_contact_ratio = 0.0, 0.0
+        print("face analysis complete")
+
+        elements = future_elements.result()
 
         student_results = get_elements_dictionary(emotion_ratio, eye_contact_ratio, elements, filler_ratio)
+        print("getting elements dictionary complete")
         print(student_results)
 
         print("Starting Evaluation")
