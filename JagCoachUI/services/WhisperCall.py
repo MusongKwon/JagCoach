9103c45b334eecda10a492ac1d9174382ee65e9d
MusongKwon <156956738+MusongKwon@users.noreply.github.com>
2025-04-16 12:33:12 -0500

    Update WhisperCall.py

--- JagCoachUI/services/WhisperCall.py
+++ JagCoachUI/services/WhisperCall.py
@@ -8,7 +8,7 @@ def get_transcript(wavPath):
 
         result = model.transcribe(wavPath, fp16=False, language="English")
         transcribed_text = result["text"]
-        
+        transcribed_text = re.sub(r'(?<!\.)\.(?!\.)', '.\n\n', transcribed_text)
         return transcribed_text
     # Otherwise, print error message
     except Exception as e:
