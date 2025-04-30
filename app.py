b6ef8af991e6570e534a0603817818b1c32db5ff
MusongKwon <156956738+MusongKwon@users.noreply.github.com>
2025-04-29 19:22:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from JagCoachUI.routes import main_bp  # Import routes from JagCoach/JagCoachUI/routes.py
+from JagCoachUI.live_routes import live_bp
 from JagCoachUI.config import config  # Import configuration
 
 app = Flask(__name__, template_folder="JagCoachUI/templates", static_folder="JagCoachUI/static")
@@ -10,6 +11,7 @@ app.config["UPLOAD_FOLDER"] = "uploads"
 
 # Register routes
 app.register_blueprint(main_bp)
+app.register_blueprint(live_bp)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=config.DEBUG)
