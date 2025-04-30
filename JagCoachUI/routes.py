8876537a0dddb943b26e6f2839f81aee8d075416
MusongKwon <156956738+MusongKwon@users.noreply.github.com>
2025-04-29 19:06:54 -0500

    Update routes.py

--- JagCoachUI/routes.py
+++ JagCoachUI/routes.py
@@ -1,11 +1,9 @@
 from flask import Blueprint, render_template, request, current_app, jsonify
 import os
-from JagCoachUI.services.JagCoachFileAnalysis import process_video, get_elements, get_elements_dictionary
+from JagCoachUI.services.JagCoachFileAnalysis import process_video, get_elements, get_feedback
 from JagCoachUI.services.FaceAnalysis import analyze_face
-from JagCoachUI.services.LLM import evaluate_speech
 from JagCoachUI.services.WhisperCall import get_transcript
 from JagCoachUI.services.FillerWords import get_filler_word_ratio
-from JagCoachUI.config import config
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 processed_audio_path = ""
@@ -16,7 +14,6 @@ processing_results = {}
 
 main_bp = Blueprint("main", __name__)
 
-@main_bp.route("/", methods=["GET", "POST"])
 @main_bp.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -29,36 +26,31 @@ def index():
 
             if os.path.exists(file_path):
                 os.remove(file_path)
-                #print(f"Existing file '{file_path}' deleted.")
 
             try:
                 file.save(file_path)
-                #print(f"Video uploaded successfully: {file_path}")
             except Exception as e:
                 print(f"Error saving file: {e}")
 
             global processed_audio_path
             processed_audio_path = process_video(file_path)
 
+            processing_results["voice_analysis"] = process_executor.submit(get_elements, processed_audio_path)
             processing_results["face_analysis"] = executor.submit(analyze_face, file_path)
-
+            
             return render_template("index.html", message=f"File '{file.filename}' uploaded successfully!",
                                    file_path=file_path)
     return render_template("index.html", message=None)
 
-
 @main_bp.route("/transcribe", methods=["POST"])
 def transcribe():
-    print(f"Transcribe has been summoned")
     try:
         global processed_audio_path
-
         transcription_text = get_transcript(processed_audio_path)
+
         global filler_ratio
         filler_ratio = get_filler_word_ratio(transcription_text)
-        print("filler ratio: ", filler_ratio)
 
-        print("Transcription complete bossman")
         return jsonify({"transcription": transcription_text})
     except Exception as e:
         print(f"Error processing audio file: {e}")
@@ -66,27 +58,24 @@ def transcribe():
 
 @main_bp.route("/evaluate", methods=["POST"])
 def evaluate():
-    print(f"Evaluate has been summoned")
     try:
-        global processed_audio_path
         global filler_ratio
-        future_elements = process_executor.submit(get_elements, processed_audio_path)
-        
+
         future = processing_results.get("face_analysis")
+        voice_analysis_future = processing_results.get("voice_analysis")
         if future:
             emotion_ratio, eye_contact_ratio = future.result()
-        print("face analysis complete")
 
-        elements = future_elements.result()
+        if voice_analysis_future:
+            elements = voice_analysis_future.result()
 
-        student_results = get_elements_dictionary(emotion_ratio, eye_contact_ratio, elements, filler_ratio)
-        print("getting elements dictionary complete")
-        print(student_results)
+        student_results = get_feedback(emotion_ratio, eye_contact_ratio, elements, filler_ratio)
 
-        print("Starting Evaluation")
-        evaluation_text = evaluate_speech(student_results)
-        print("Evaluation complete bossman")
-        return jsonify({"evaluation": evaluation_text})
+        return jsonify({"evaluation": student_results})
     except Exception as e:
         print(f"Error processing audio file: {e}")
         return jsonify({"error": str(e)}), 500
+    
+@main_bp.route("/present-live", methods=["GET"])
+def present_live():
+    return render_template("present_live.html")
